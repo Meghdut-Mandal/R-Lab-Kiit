b7d514d1bb52ed549d9fd6d6dd58979e5ca7488a
vijay-gautam <mailforvijay3@gmail.com>
2020-09-29 09:51:08 +0530

    ques1

---
+++ Lab6/q1.R
@@ -0,0 +1,11 @@
+players <- data.frame(
+  player_no = c(1:5),
+  name = c("Meghdut","Messi","Michael Jordan","Rohit Sharma","Ravi Shastri"),
+  age = c(17,31,37,29,48),
+  profession= c("Sprinter","Footballer","Cricketer","Cricketer","Alcoholic"),
+  grade = c("C","A","B","A","A+")
+) 
+
+print(players)
+str(players)
+print(summary(players))
