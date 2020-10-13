4a1306a77c471ca4d6ce85bea8143b0e3a49cbf9
curatorRaj <1828043@kiit.ac.in>
2020-10-13 09:31:14 +0530

    NA value

--- lab7/q1.R
+++ lab7/q1.R
@@ -4,10 +4,7 @@
 # Created on: 06-10-2020
 
 housingdata <- read.csv("lab7/Boston.csv")
-#print(housingdata)
 
-
-#str(housingdata)
 for (i in colnames(housingdata)){
   cat(i , " ", sum(is.na(housingdata[[i]])), "\n")
 }
