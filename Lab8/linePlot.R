c23db74347f4bedbbb7e29a855fefa991ba05473
vijay-gautam <mailforvijay3@gmail.com>
2020-10-13 10:36:32 +0530

    lab 8

--- Lab8/linePlot.R
+++ Lab8/linePlot.R
@@ -1,10 +1,8 @@
-# Title     : TODO
-# Objective : TODO
-# Created by: KIIT
-# Created on: 13-10-2020
-
 data <- read.csv('Lab8/data.csv')
+summary(data)
 data[data == "null"] <- NA
+data <- na.omit(data)
+summary(data)
 png('Lab8/infosys.jpg')
 plot(data$Open,type = "o", col = "blue", xlab = "Index", ylab = "Open/Close",main = "Stock Price")
 lines(data$Close, type = "o", col ="red")
